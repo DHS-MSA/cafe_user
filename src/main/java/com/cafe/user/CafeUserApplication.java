c73a15ac573338cb0f03f7e8f691cad6c8b897af
chaesuhyeon <ddd8177@gmail.com>
2023-02-14 22:14:05 +0900

    initial commit

---
+++ src/main/java/com/cafe/user/CafeUserApplication.java
@@ -0,0 +1,13 @@
+package com.cafe.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeUserApplication.class, args);
+	}
+
+}
