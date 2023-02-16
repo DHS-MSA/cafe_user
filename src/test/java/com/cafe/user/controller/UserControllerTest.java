729e0a908bc2236691772ea87b5e32f00e2d6ce5
chaesuhyeon <ddd8177@gmail.com>
2023-02-16 22:53:50 +0900

    get user info - response api docs 설정 & 커스텀 완료

--- src/test/java/com/cafe/user/controller/UserControllerTest.java
+++ src/test/java/com/cafe/user/controller/UserControllerTest.java
@@ -9,12 +9,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
@@ -37,17 +39,17 @@ class UserControllerTest {
         )
                 .andExpect(status().isOk())
                 .andDo(// 문서 작성 시작
-                        document("get-user-info",
+                        document("get-user-info", preprocessResponse(prettyPrint()),
                                 pathParameters(
-                                        parameterWithName("userId").description("user id")
-                                ),
+                                        parameterWithName("userId").description("사용자 아이디")
+                                )
+                                ,
                                 responseFields(
-                                        fieldWithPath("userId").description("사용자 아이디"),
-                                        fieldWithPath("name").description("사용자 이름"),
-                                        fieldWithPath("address").description("사용자 주소"),
-                                        fieldWithPath("phoneNumber").description("사용자 번호"),
-                                        fieldWithPath("createdAt").description("사용자 가입일")
-                                ))
-                );
+                                        fieldWithPath("userId").type(JsonFieldType.STRING).description("사용자 아이디"),
+                                        fieldWithPath("name").type(JsonFieldType.STRING).description("사용자 이름").optional(),
+                                        fieldWithPath("address").type(JsonFieldType.STRING).description("사용자 주소").optional(),
+                                        fieldWithPath("phoneNumber").type(JsonFieldType.STRING).description("사용자 번호").optional(),
+                                        fieldWithPath("createdAt").type(JsonFieldType.STRING).description("사용자 가입일").optional()
+                                )));
     }
 }
